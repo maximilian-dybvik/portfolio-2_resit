d896d4a33f747e15d077b8d8da334ff82805cefc
Maximilian Dybvik <maximilian.dybvik@gmail.com>
2019-08-31 22:24:56 +0200

    added intaractivity to canvas

--- javascript_2/dist/build.app.js
+++ javascript_2/dist/build.app.js
@@ -8,57 +8,77 @@ document.addEventListener('DOMContentLoaded', function (event) {
         y: undefined
     };
     window.addEventListener('mousemove', function (event) {
-        console.log(event);
+        mousePosition.x = event.x;
+        mousePosition.y = event.y;
     });
-    // let minRadius: number = 3 // Determines the size of the shapes
-    // let maxRadius: number = 10 // Determines the size of the shapes
-    var radius = 10; // Determines the size of the shapes
+    var minRadius = 3; // Determines the size of the shapes
+    var maxRadius = 15; // Determines the size of the shapes
+    var growthFactor = 5;
+    var interactionRadius = 60;
     var velocity = 3; // Determines the movement speed of the shapes
     var randomizationRange = 3; // Range of randomization to velocity, adds a random number between negative and positive randonizationRange to velocity
-    var density = 100; // Determines the number of shapes drawn to canvas
-    var colors = [];
-    var Shape = function (x, y, dx, dy, radius) {
+    var density = 300; // Determines the number of shapes drawn to canvas
+    var colors = [
+        '#02bacf',
+        '#fb9100',
+        '#dcc9a9',
+        '#828282',
+        '#4f545b'
+    ];
+    var Shape = function (x, y, xv, yv, radius, colors) {
         var _this = this;
         this.radius = radius;
+        this.minRadius = radius;
+        this.maxRadius = radius;
         this.x = x;
         this.y = y;
-        this.dx = dx;
-        this.dy = dy;
+        this.xv = xv;
+        this.yv = yv;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
         this.draw = function () {
             ctx.beginPath();
             ctx.arc(_this.x, _this.y, _this.radius, 0, Math.PI * 2, false);
             ctx.lineWidth = 5;
             ctx.strokeStyle = 'white';
             ctx.stroke();
-            ctx.fillStyle = 'black';
+            ctx.fillStyle = _this.color;
             ctx.fill();
         };
         this.update = function () {
             if (_this.x + _this.radius > innerWidth || _this.x - _this.radius < 0) {
-                _this.dx = -_this.dx;
+                _this.xv = -_this.xv;
             }
             if (_this.y + _this.radius > innerHeight || _this.y - _this.radius < 0) {
-                _this.dy = -_this.dy;
+                _this.yv = -_this.yv;
             }
-            _this.x += _this.dx;
-            _this.y += _this.dy;
-            // Interactivity around mouse position
-            console.log(mousePosition.x);
+            _this.x += _this.xv;
+            _this.y += _this.yv;
+            if (mousePosition.x - _this.x < interactionRadius
+                && mousePosition.x - _this.x > -interactionRadius
+                && mousePosition.y - _this.y < interactionRadius
+                && mousePosition.y - _this.y > -interactionRadius) {
+                if (_this.radius < _this.minRadius * growthFactor) {
+                    _this.radius += 1;
+                }
+            }
+            else if (_this.radius > _this.minRadius) {
+                _this.radius -= 1;
+            }
             _this.draw();
         };
     };
     var shapesArray = [];
     for (var i = 0; i < density; i++) {
-        // Determines the initial direction of movement for the x and y axis
-        var xDirection = Math.round(Math.random()) * 2 - 1;
-        var yDirection = Math.round(Math.random()) * 2 - 1;
+        var radius = Math.random() * (maxRadius - minRadius) + minRadius;
+        // Determines the initial direction of movement for the x and y axis by returning either -1 or 1
+        var direction = function () { return Math.round(Math.random()) * 2 - 1; };
         // Determines initial draw position for the x and y axis
         var x = Math.random() * (innerWidth - radius * 2) + radius;
         var y = Math.random() * (innerHeight - radius * 2) + radius;
         // Determines the movement speed based on velocity and randomization range
-        var dx = (velocity - Math.random() * randomizationRange) * xDirection;
-        var dy = (velocity - Math.random() * randomizationRange) * yDirection;
-        shapesArray.push(new Shape(x, y, dx, dy, radius));
+        var xv = (velocity - Math.random() * randomizationRange) * direction();
+        var yv = (velocity - Math.random() * randomizationRange) * direction();
+        shapesArray.push(new Shape(x, y, xv, yv, radius, colors));
     }
     var animate = function () {
         requestAnimationFrame(animate);
@@ -67,6 +87,5 @@ document.addEventListener('DOMContentLoaded', function (event) {
             shapesArray[i].update();
         }
     };
-    console.log(shapesArray[0]);
     animate();
 }, false);
--- javascript_2/src/ts/app.ts
+++ javascript_2/src/ts/app.ts
@@ -11,23 +11,34 @@ document.addEventListener('DOMContentLoaded', function (event) {
   }
 
   window.addEventListener('mousemove', (event) => {
-    console.log(event);
+    mousePosition.x = event.x
+    mousePosition.y = event.y
   })
 
-  // let minRadius: number = 3 // Determines the size of the shapes
-  // let maxRadius: number = 10 // Determines the size of the shapes
-  let radius: number = 10 // Determines the size of the shapes
+  let minRadius: number = 3 // Determines the size of the shapes
+  let maxRadius: number = 15 // Determines the size of the shapes
+  let growthFactor: number = 5
+  let interactionRadius: number = 60
   let velocity: number = 3 // Determines the movement speed of the shapes
   let randomizationRange: number = 3 // Range of randomization to velocity, adds a random number between negative and positive randonizationRange to velocity
-  let density: number = 100 // Determines the number of shapes drawn to canvas
-  let colors: Array<string> = []
+  let density: number = 300 // Determines the number of shapes drawn to canvas
+  let colors: Array<string> = [
+    '#02bacf',
+    '#fb9100',
+    '#dcc9a9',
+    '#828282',
+    '#4f545b'
+  ]
 
-  const Shape = function (x, y, dx, dy, radius) {
+  const Shape = function (x, y, xv, yv, radius, colors) {
     this.radius = radius
+    this.minRadius = radius
+    this.maxRadius = radius
     this.x = x
     this.y = y
-    this.dx = dx
-    this.dy = dy
+    this.xv = xv
+    this.yv = yv
+    this.color = colors[Math.floor(Math.random() * colors.length)]
 
     this.draw = () => {
       ctx.beginPath()
@@ -35,45 +46,51 @@ document.addEventListener('DOMContentLoaded', function (event) {
       ctx.lineWidth = 5
       ctx.strokeStyle = 'white'
       ctx.stroke()
-      ctx.fillStyle = 'black'
+      ctx.fillStyle = this.color
       ctx.fill()
     }
 
     this.update = () => {
       if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-        this.dx = -this.dx
+        this.xv = -this.xv
       }
   
       if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-        this.dy = -this.dy
+        this.yv = -this.yv
       }
   
-      this.x += this.dx
-      this.y += this.dy
+      this.x += this.xv
+      this.y += this.yv
 
-
-      // Interactivity around mouse position
-      console.log(mousePosition.x);
-      
-
-      this.draw()
+    if (mousePosition.x - this.x < interactionRadius 
+      && mousePosition.x - this.x > -interactionRadius 
+      && mousePosition.y - this.y < interactionRadius 
+      && mousePosition.y - this.y > -interactionRadius) {
+        if (this.radius < this.minRadius * growthFactor) {
+          this.radius += 1
+        }
+    } else if (this.radius > this.minRadius) {
+        this.radius -= 1
+    }
+    
+     this.draw()
     }
   }
 
   let shapesArray: Array<any> = []
 
   for (let i: number = 0; i < density; i++) {
-    // Determines the initial direction of movement for the x and y axis
-    let xDirection: number = Math.round(Math.random()) * 2 - 1
-    let yDirection: number = Math.round(Math.random()) * 2 - 1
+    let radius: number = Math.random() * (maxRadius - minRadius) + minRadius
+    // Determines the initial direction of movement for the x and y axis by returning either -1 or 1
+    const direction = () => Math.round(Math.random()) * 2 - 1
     // Determines initial draw position for the x and y axis
     let x: number = Math.random() * (innerWidth - radius * 2) + radius
     let y: number = Math.random() * (innerHeight - radius * 2) + radius
     // Determines the movement speed based on velocity and randomization range
-    let dx: number = (velocity - Math.random() * randomizationRange) * xDirection
-    let dy: number = (velocity - Math.random() * randomizationRange) * yDirection
+    let xv: number = (velocity - Math.random() * randomizationRange) * direction()
+    let yv: number = (velocity - Math.random() * randomizationRange) * direction()
     
-    shapesArray.push(new Shape(x, y, dx, dy, radius))
+    shapesArray.push(new Shape(x, y, xv, yv, radius, colors))
   }
 
   const animate = function () {
@@ -85,7 +102,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
   }
 
-  console.log(shapesArray[0]);
   animate()
 }, false)
 
