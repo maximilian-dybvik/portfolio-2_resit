15cab559cf447a137a85968eafbf1e1e1d482013
Maximilian Dybvik <maximilian.dybvik@gmail.com>
2019-09-01 23:25:10 +0200

    made fully interactive, and removed velocity bug causing valocity to increace exponentially on window resize

--- javascript_2/dist/build.app.js
+++ javascript_2/dist/build.app.js
@@ -1,91 +1,128 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-    var canvas = document.querySelector('.canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    var ctx = canvas.getContext('2d');
-    var mousePosition = {
-        x: undefined,
-        y: undefined
-    };
-    window.addEventListener('mousemove', function (event) {
-        mousePosition.x = event.x;
-        mousePosition.y = event.y;
-    });
-    var minRadius = 3; // Determines the size of the shapes
-    var maxRadius = 15; // Determines the size of the shapes
-    var growthFactor = 5;
-    var interactionRadius = 60;
-    var velocity = 3; // Determines the movement speed of the shapes
-    var randomizationRange = 3; // Range of randomization to velocity, adds a random number between negative and positive randonizationRange to velocity
-    var density = 300; // Determines the number of shapes drawn to canvas
-    var colors = [
+// Config variables, change these to play with canvas
+var config = {
+    minRadius: 1,
+    maxRadius: 5,
+    growthFactor: 5,
+    growthRate: 2,
+    interactionRadius: 50,
+    minVelocity: 0,
+    maxVelocity: 1,
+    density: 800,
+    // Each shape drawn to canvas gets assigned a random color from this array
+    colors: [
         '#02bacf',
         '#fb9100',
         '#dcc9a9',
         '#828282',
         '#4f545b'
-    ];
-    var Shape = function (x, y, xv, yv, radius, colors) {
-        var _this = this;
-        this.radius = radius;
-        this.minRadius = radius;
-        this.maxRadius = radius;
-        this.x = x;
-        this.y = y;
-        this.xv = xv;
-        this.yv = yv;
-        this.color = colors[Math.floor(Math.random() * colors.length)];
-        this.draw = function () {
-            ctx.beginPath();
+    ],
+    stroke: false,
+    strokeWidth: 2,
+    strokeColor: '#fffdfa',
+    shape: 'circle' // Defines what shape to draw on canvas (options: circle, square). Defaults to circle on any other input
+};
+// Each index of this array holds a new instance of Shape
+var shapesArray = [];
+// Object for storing x and y mouse coordinates whitin the browser window
+var mousePosition = {
+    x: undefined,
+    y: undefined
+};
+// Setup canvas element and define its size
+var canvas = document.querySelector('.canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// Define rendering context for out canvas to 2D
+var ctx = canvas.getContext('2d');
+// Reinitiate and adjust canvas size when browser window is resized
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+});
+// Keep track os mouse position and stores x and y coordinates in mousePosition object
+window.addEventListener('mousemove', function (event) {
+    mousePosition.x = event.x;
+    mousePosition.y = event.y;
+});
+// Shape constructor used to generate unike shapes based on the config object
+var Shape = function (x, y, xv, yv, radius, colors) {
+    var _this = this;
+    this.radius = radius; // The radius of this shape
+    this.minRadius = radius; // The radius which this shape shrinks back to after interaction
+    this.maxRadius = radius; // The radius which this shape grows to on interaction
+    this.x = x; // The initial x coordinate this shape gets drawn on
+    this.y = y; // The initial y coordinate this shape gets drawn on
+    this.xv = xv; // This shapes velocity along the x axis
+    this.yv = yv; // This shapes velocity along the y axis
+    this.color = colors[Math.floor(Math.random() * colors.length)]; // Picks a random color from the colors array
+    this.draw = function () {
+        ctx.beginPath();
+        // Determine what shape to draw on canvas
+        if (config.shape === 'square') {
+            ctx.rect(_this.x, _this.y, _this.radius * 2, _this.radius * 2);
+        }
+        else {
             ctx.arc(_this.x, _this.y, _this.radius, 0, Math.PI * 2, false);
-            ctx.lineWidth = 5;
-            ctx.strokeStyle = 'white';
+        }
+        // Checks config object whether stroke is true or false
+        if (config.stroke) {
+            ctx.lineWidth = config.strokeWidth;
+            ctx.strokeStyle = config.strokeColor;
             ctx.stroke();
-            ctx.fillStyle = _this.color;
-            ctx.fill();
-        };
-        this.update = function () {
-            if (_this.x + _this.radius > innerWidth || _this.x - _this.radius < 0) {
-                _this.xv = -_this.xv;
-            }
-            if (_this.y + _this.radius > innerHeight || _this.y - _this.radius < 0) {
-                _this.yv = -_this.yv;
-            }
-            _this.x += _this.xv;
-            _this.y += _this.yv;
-            if (mousePosition.x - _this.x < interactionRadius
-                && mousePosition.x - _this.x > -interactionRadius
-                && mousePosition.y - _this.y < interactionRadius
-                && mousePosition.y - _this.y > -interactionRadius) {
-                if (_this.radius < _this.minRadius * growthFactor) {
-                    _this.radius += 1;
-                }
-            }
-            else if (_this.radius > _this.minRadius) {
-                _this.radius -= 1;
-            }
-            _this.draw();
-        };
+        }
+        ctx.fillStyle = _this.color;
+        ctx.fill();
     };
-    var shapesArray = [];
-    for (var i = 0; i < density; i++) {
-        var radius = Math.random() * (maxRadius - minRadius) + minRadius;
-        // Determines the initial direction of movement for the x and y axis by returning either -1 or 1
+    this.update = function () {
+        // Changes this shapes direction when it hits the right or left edge of the browser window
+        if (_this.x + _this.radius > innerWidth || _this.x - _this.radius < 0) {
+            _this.xv = -_this.xv;
+        }
+        // Changes this shapes direction when it hits the top or bottom edge of the browser window
+        if (_this.y + _this.radius > innerHeight || _this.y - _this.radius < 0) {
+            _this.yv = -_this.yv;
+        }
+        // As long as this shape is within the browser window continue moving in same direction
+        _this.x += _this.xv;
+        _this.y += _this.yv;
+        // Adds interactivity around mouse position
+        if (mousePosition.x - _this.x < config.interactionRadius
+            && mousePosition.x - _this.x > -config.interactionRadius
+            && mousePosition.y - _this.y < config.interactionRadius
+            && mousePosition.y - _this.y > -config.interactionRadius) {
+            if (_this.radius < _this.minRadius * config.growthFactor) {
+                _this.radius += config.growthRate;
+            }
+        }
+        else if (_this.radius > _this.minRadius) {
+            _this.radius -= config.growthRate;
+        }
+        _this.draw();
+    };
+};
+// Initiate the canvas by instanciating shapes and pushing them to shapesArray
+var init = function () {
+    shapesArray = [];
+    for (var i = 0; i < config.density; i++) {
+        // This function returns either 1 or -1, used to set the initial direction of each shape
         var direction = function () { return Math.round(Math.random()) * 2 - 1; };
-        // Determines initial draw position for the x and y axis
+        var radius = Math.round(Math.random() * (config.maxRadius - config.minRadius) + config.minRadius);
         var x = Math.random() * (innerWidth - radius * 2) + radius;
         var y = Math.random() * (innerHeight - radius * 2) + radius;
-        // Determines the movement speed based on velocity and randomization range
-        var xv = (velocity - Math.random() * randomizationRange) * direction();
-        var yv = (velocity - Math.random() * randomizationRange) * direction();
-        shapesArray.push(new Shape(x, y, xv, yv, radius, colors));
+        var xv = (Math.random() * (config.maxVelocity - config.minVelocity) + config.minVelocity) * direction();
+        var yv = (Math.random() * (config.maxVelocity - config.minVelocity) + config.minVelocity) * direction();
+        shapesArray.push(new Shape(x, y, xv, yv, radius, config.colors));
     }
-    var animate = function () {
-        requestAnimationFrame(animate);
-        ctx.clearRect(0, 0, innerWidth, innerHeight);
-        for (var i = 0; i < shapesArray.length; i++) {
-            shapesArray[i].update();
-        }
-    };
-    animate();
-}, false);
+};
+// Request animation frame and draw/update shapes from shapesArray
+var animate = function () {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    for (var i = 0; i < shapesArray.length; i++) {
+        shapesArray[i].update();
+    }
+};
+// Initial init and animate calls
+init();
+animate();
--- javascript_2/src/ts/app.ts
+++ javascript_2/src/ts/app.ts
@@ -1,108 +1,161 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-  const canvas: HTMLCanvasElement = document.querySelector('.canvas')
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
+// Defines a new type in typescript so we can type check our key:values in our config object
+interface ConfigObject {
+  minRadius: number|undefined
+  maxRadius: number|undefined
+  growthFactor: number|undefined
+  growthRate: number|undefined
+  interactionRadius: number|undefined
+  minVelocity: number|undefined
+  maxVelocity: number|undefined
+  density: number|undefined
+  colors: Array<string>|undefined
+  stroke: boolean|undefined
+  strokeWidth: number|undefined
+  strokeColor: string|undefined
+  shape: string|undefined
+}
 
-  const ctx: CanvasRenderingContext2D = canvas.getContext('2d')
-
-  let mousePosition = {
-    x: undefined,
-    y: undefined
-  }
-
-  window.addEventListener('mousemove', (event) => {
-    mousePosition.x = event.x
-    mousePosition.y = event.y
-  })
-
-  let minRadius: number = 3 // Determines the size of the shapes
-  let maxRadius: number = 15 // Determines the size of the shapes
-  let growthFactor: number = 5
-  let interactionRadius: number = 60
-  let velocity: number = 3 // Determines the movement speed of the shapes
-  let randomizationRange: number = 3 // Range of randomization to velocity, adds a random number between negative and positive randonizationRange to velocity
-  let density: number = 300 // Determines the number of shapes drawn to canvas
-  let colors: Array<string> = [
+// Config variables, change these to play with canvas
+let config: ConfigObject = {
+  minRadius: 1, // Smallest radius drawn on canvas
+  maxRadius: 5, // Largest raduis drawn on canvas
+  growthFactor: 5, // A multiplier determinig the size each shape grows within the interaction radius
+  growthRate: 2, // The rate each shape grows within the interaction radius
+  interactionRadius: 50, // The radius around your mouse which triggers interaction
+  minVelocity: 0, // Minimum movement speed for each shape in either x or y direction
+  maxVelocity: 1, // Maximum movement speed for each shape in either x or y direction
+  density: 800, // The amount of shapes drawn to canvas
+  // Each shape drawn to canvas gets assigned a random color from this array
+  colors: [
     '#02bacf',
     '#fb9100',
     '#dcc9a9',
     '#828282',
     '#4f545b'
-  ]
+  ],
+  stroke: false, // Toggle stroke effect on of off (false means off)
+  strokeWidth: 2, // The width of the stroke
+  strokeColor: '#fffdfa', // The color of the stroke
+  shape: 'circle' // Defines what shape to draw on canvas (options: circle, square). Defaults to circle on any other input
+}
 
-  const Shape = function (x, y, xv, yv, radius, colors) {
-    this.radius = radius
-    this.minRadius = radius
-    this.maxRadius = radius
-    this.x = x
-    this.y = y
-    this.xv = xv
-    this.yv = yv
-    this.color = colors[Math.floor(Math.random() * colors.length)]
+// Each index of this array holds a new instance of Shape
+let shapesArray: Array<any> = []
 
-    this.draw = () => {
-      ctx.beginPath()
+// Object for storing x and y mouse coordinates whitin the browser window
+let mousePosition = {
+  x: undefined,
+  y: undefined
+}
+
+// Setup canvas element and define its size
+const canvas: HTMLCanvasElement = document.querySelector('.canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+// Define rendering context for out canvas to 2D
+const ctx: CanvasRenderingContext2D = canvas.getContext('2d')
+
+// Reinitiate and adjust canvas size when browser window is resized
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  init()
+})
+
+// Keep track os mouse position and stores x and y coordinates in mousePosition object
+window.addEventListener('mousemove', (event) => {
+  mousePosition.x = event.x
+  mousePosition.y = event.y
+})
+
+// Shape constructor used to generate unike shapes based on the config object
+const Shape = function (x, y, xv, yv, radius, colors) {
+  this.radius = radius // The radius of this shape
+  this.minRadius = radius // The radius which this shape shrinks back to after interaction
+  this.maxRadius = radius // The radius which this shape grows to on interaction
+  this.x = x // The initial x coordinate this shape gets drawn on
+  this.y = y // The initial y coordinate this shape gets drawn on
+  this.xv = xv // This shapes velocity along the x axis
+  this.yv = yv // This shapes velocity along the y axis
+  this.color = colors[Math.floor(Math.random() * colors.length)] // Picks a random color from the colors array
+
+  this.draw = () => {
+    ctx.beginPath()
+    // Determine what shape to draw on canvas
+    if (config.shape === 'square') {
+      ctx.rect(this.x, this.y, this.radius * 2, this.radius * 2)
+    } else {
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-      ctx.lineWidth = 5
-      ctx.strokeStyle = 'white'
+    }
+    // Checks config object whether stroke is true or false
+    if (config.stroke) {
+      ctx.lineWidth = config.strokeWidth
+      ctx.strokeStyle = config.strokeColor
       ctx.stroke()
-      ctx.fillStyle = this.color
-      ctx.fill()
+    }
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+
+  this.update = () => {
+    // Changes this shapes direction when it hits the right or left edge of the browser window
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.xv = -this.xv
     }
 
-    this.update = () => {
-      if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-        this.xv = -this.xv
-      }
-  
-      if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-        this.yv = -this.yv
-      }
-  
-      this.x += this.xv
-      this.y += this.yv
+    // Changes this shapes direction when it hits the top or bottom edge of the browser window
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.yv = -this.yv
+    }
 
-    if (mousePosition.x - this.x < interactionRadius 
-      && mousePosition.x - this.x > -interactionRadius 
-      && mousePosition.y - this.y < interactionRadius 
-      && mousePosition.y - this.y > -interactionRadius) {
-        if (this.radius < this.minRadius * growthFactor) {
-          this.radius += 1
+    // As long as this shape is within the browser window continue moving in same direction
+    this.x += this.xv
+    this.y += this.yv
+
+    // Adds interactivity around mouse position
+    if (mousePosition.x - this.x < config.interactionRadius 
+      && mousePosition.x - this.x > -config.interactionRadius 
+      && mousePosition.y - this.y < config.interactionRadius 
+      && mousePosition.y - this.y > -config.interactionRadius) {
+        if (this.radius < this.minRadius * config.growthFactor) {
+          this.radius += config.growthRate
         }
     } else if (this.radius > this.minRadius) {
-        this.radius -= 1
+        this.radius -= config.growthRate
     }
-    
-     this.draw()
-    }
+  
+    this.draw()
   }
+}
 
-  let shapesArray: Array<any> = []
-
-  for (let i: number = 0; i < density; i++) {
-    let radius: number = Math.random() * (maxRadius - minRadius) + minRadius
-    // Determines the initial direction of movement for the x and y axis by returning either -1 or 1
+// Initiate the canvas by instanciating shapes and pushing them to shapesArray
+const init = function () {
+  shapesArray = []
+  for (let i: number = 0; i < config.density; i++) {
+    // This function returns either 1 or -1, used to set the initial direction of each shape
     const direction = () => Math.round(Math.random()) * 2 - 1
-    // Determines initial draw position for the x and y axis
+
+    let radius: number = Math.round(Math.random() * (config.maxRadius - config.minRadius) + config.minRadius)
     let x: number = Math.random() * (innerWidth - radius * 2) + radius
     let y: number = Math.random() * (innerHeight - radius * 2) + radius
-    // Determines the movement speed based on velocity and randomization range
-    let xv: number = (velocity - Math.random() * randomizationRange) * direction()
-    let yv: number = (velocity - Math.random() * randomizationRange) * direction()
-    
-    shapesArray.push(new Shape(x, y, xv, yv, radius, colors))
+    let xv: number = (Math.random() * (config.maxVelocity - config.minVelocity) + config.minVelocity) * direction()
+    let yv: number = (Math.random() * (config.maxVelocity - config.minVelocity) + config.minVelocity) * direction()
+
+    shapesArray.push(new Shape(x, y, xv, yv, radius, config.colors))
   }
+}
 
-  const animate = function () {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, innerWidth, innerHeight)
+// Request animation frame and draw/update shapes from shapesArray
+const animate = function () {
+  requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, innerWidth, innerHeight)
 
-    for (let i: number = 0; i < shapesArray.length; i++) {
-      shapesArray[i].update()
-    }
+  for (let i: number = 0; i < shapesArray.length; i++) {
+    shapesArray[i].update()
   }
+}
 
-  animate()
-}, false)
-
-
+// Initial init and animate calls
+init()
+animate()
